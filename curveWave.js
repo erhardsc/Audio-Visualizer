7fd323780de5457fbf1ac1d8ab87037dc15da4a5
Sumner <serhard2556@gmail.com>
2017-09-28 18:00:10 -0600

    Using bezierVertex now

--- curveWave.js
+++ curveWave.js
@@ -10,27 +10,34 @@ function CurveWave(color, limiter){
 
         for (var i = 0; i < this.spectrum.length; i++) {
             this.frequency = this.spectrum[i];
-            this.amp = map(this.frequency * this.limiter, 0, 256, 200, 3000);
+            this.ampX = map(this.frequency, 0, 256, -10, 1000);
+            this.ampY = map(this.frequency * this.limiter, 0, 256, 300, 3000);
 
-            return this.amp;
+            return [this.ampX, this.ampY];
         }
     }
 
     this.show = function () {
+
+        push(); // Save state
+
         noStroke();
         fill(this.color);
         this.position();
+
         beginShape();
-        /*2*/curveVertex(0, this.amplitude());
+        /*2*/curveVertex(this.amplitude()[0], this.amplitude()[1]); // Adjust angle of curve
         /*2*/curveVertex(0, 50);
         /*3*/curveVertex(width, 60);
         /*4*/curveVertex(width, height);
         /*1*/curveVertex(0, height);
         /*1*/curveVertex(0, height);
         endShape();
+
+        pop(); // Restore state
     }
 
     this.position = function () {
-        translate(0, height/2);
+        translate(0, height * 0.8);
     }
 }
--- sketch.js
+++ sketch.js
@@ -24,16 +24,16 @@ function setup() {
     var myCanvas = createCanvas(window.innerWidth, window.innerHeight);
     myCanvas.parent('visualizer');
 
-    fft = new p5.FFT(0.95, 64);
+    fft = new p5.FFT();
+    fft.smooth(0.95);
 
-    var dark = color('rgba(22, 25, 37, .85)');
-    var grey = color('rgb(142, 137, 137)');
+    var dark = color('rgba(11, 10, 10, .65)');
+    var grey = color('rgba(142, 137, 137, .65)');
     var purple = color('rgb(106, 63, 190)');
-    var lightPurple = color('rgb(200, 109, 215)');
 
-    wave = new CurveWave(purple, 1.2);
-    wave2 = new CurveWave(grey, 2);
-    wave3 = new CurveWave(dark, 4);
+    wave = new CurveWave(purple, 4);
+    wave2 = new CurveWave(grey, 3);
+    wave3 = new CurveWave(dark, 2);
 
     button = createButton('toggle');
     button.mousePressed(toggleSong);
@@ -44,12 +44,67 @@ function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
 }
 
-function draw() {
-    background(0);
-    // Generate Waves. Defaults to stacking each element
+function amplitude(){
+    var spectrum = fft.analyze();
 
-    wave.show();
-    wave2.show();
-    wave3.show();
+    for (var i = 0; i < spectrum.length; i++) {
+        var frequency = spectrum[i];
+        var ampX = map(frequency, 0, 256, 400, 0);
+        var ampY = map(frequency, 0, 256, 200, 300);
+        var ampW = map(frequency, 0, 256, 200, 300);
+        var ampZ = map(frequency, 0, 256, 400, 200);
+
+
+        return [ampX, ampY, ampZ];
+    }
+}
+
+function draw() {
+    background(255);
+    // Generate Waves. Defaults to stacking each element
+    // wave.show();
+    // wave2.show();
+    //
+    // wave3.show();
+
+
+    // stroke(0);
+    // // fill(0);
+    //
+    // strokeWeight(4);
+    // /*1*/point(100, amplitude());
+    // /*1*/point(100, 50);
+    //
+    // /*2*/point(250, 60);
+    //
+    // /*3*/point(300, 60);
+    //
+    // /*4*/point(400, 60);
+    // /*4*/point(300, 200);
+    // strokeWeight(1);
+    //
+    //
+    //
+    // beginShape();
+    // /*1*/curveVertex(100, amplitude());
+    // /*1*/curveVertex(100, 50);
+    //
+    // /*2*/curveVertex(250, 60);
+    //
+    // /*3*/curveVertex(300, 60);
+    // /*3*/curveVertex(300, 60);
+    //
+    // /*4*/curveVertex(400, 60);
+    //
+    // /*5*/curveVertex(300, amplitude() / 2);
+    // /*5*/curveVertex(300, 200);
+    // endShape();
+    translate(width /2, height /2);
+    beginShape();
+    vertex(493, 396);
+    vertex(10, 398);
+    vertex(10, 300);
+    bezierVertex(amplitude()[0], 0, amplitude()[2], amplitude()[1], 493, 300);
+    endShape()
 
 }
