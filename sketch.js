0e589d773962ad9627975118ae8012431c831d07
Sumner <serhard2556@gmail.com>
2017-09-30 17:25:20 -0600

    weird wave animation with spotlight feature

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 
 p5.disableFriendlyErrors = true;
+
 var song, fft, button, radius;
+
+var ctx;
 
 var wave;
 var wave2;
@@ -24,6 +27,8 @@ function setup() {
 
     var myCanvas = createCanvas(window.innerWidth, window.innerHeight);
     myCanvas.parent('visualizer');
+
+    ctx = myCanvas.drawingContext;
 
     fft = new p5.FFT();
     fft.smooth(0.95);
@@ -49,7 +54,7 @@ function amplitude(){
     var spectrum = fft.analyze();
 
     for (var i = 0; i < spectrum.length; i++) {
-        var amp = map(spectrum[i], 0, 256, height*19/32, 600);
+        var amp = map(spectrum[i], 0, 256, 600, 0);
 
 
         return amp;
@@ -57,7 +62,7 @@ function amplitude(){
 }
 
 function draw() {
-    background(255);
+    background(50, 0, 150);
     // Draw FPS (rounded to 2 decimal places) at the bottom left of the screen
     var fps = frameRate();
     fill(0);
@@ -87,6 +92,14 @@ function draw() {
     // scale(-1,1);
     // wave3.show();
     // pop();
+
+    var gradX = mouseX - width / 2;
+    var gradY = mouseY - height / 2;
+    var gradient = ctx.createRadialGradient(0, 0, 50, gradX, gradY, 0);
+    gradient.addColorStop(0, "black");
+    gradient.addColorStop(1, "white");
+    ctx.fillStyle = gradient;
+
     strokeWeight(4);
     //point(width*.57,height*.8);
      //point(width*.8,height*.91);
@@ -96,17 +109,17 @@ function draw() {
     //  line(width * .98,height*.7, width*.94,height*.67);
 
     beginShape();
-    noFill();
+
     vertex(10,height *.98);
     vertex(10,height * .8);
-    bezierVertex(width*.05,height*.6,  width*.1,height*.6,   width*.14,height*.65);//peak1-left
-    bezierVertex(width*.2,height*.75,  width*.2,height*.88,   width*.3,height*.94);//peak1-right
+    bezierVertex(width*.05,height*.6,  width*.1,amplitude(),   width*.14,amplitude());//peak1-left
+    bezierVertex(width*.2,amplitude(),  width*.2,height*.88,   width*.3,height*.94);//peak1-right
 
-    bezierVertex(width*.4,height*.95,  width*.4,height*.8,   width*.55,height*.82);//peak2-left
-    bezierVertex(width*.62,height*.82,  width*.62,height*.95,   width*.8,height*.89);//peak2-right
+    bezierVertex(width*.4,height*.95,  width*.4,amplitude(),   width*.55,amplitude() * 1.5);//peak2-left
+    bezierVertex(width*.62,amplitude() * 1.5,  width*.62,height*.95,   width*.8,height*.89);//peak2-right
 
-    bezierVertex(width*.95,height*.78,  width*.8,height*.65,   width*.93,height*.6);//peak3-left
-    bezierVertex(width*.97,height*.61,  width*.96,height*.67,   width * .98,height*.7);//peak3-right
+    bezierVertex(width*.95,height*.78,  width*.8,amplitude(),   width*.93,amplitude());//peak3-left
+    bezierVertex(width*.97,amplitude(),  width*.96,height*.67,   width * .98,height*.7);//peak3-right
     vertex(width * .98,height *.98);
 
     endShape();
