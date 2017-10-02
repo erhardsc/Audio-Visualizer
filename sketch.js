908c306f4d577258d38bd9961fad566a66638a8d
Sumner <serhard2556@gmail.com>
2017-10-02 16:48:03 -0600

    BezierVertex Visualizer prototype. Also, adding gradient color scheme significantly slows down the FPS to about 1 FPS on mobile devices. Might need to revert back to a simpler visualizer for mobile devices

--- sketch.js
+++ sketch.js
@@ -54,104 +54,87 @@ function amplitude(){
     var spectrum = fft.analyze();
 
     for (var i = 0; i < spectrum.length; i++) {
-        var amp = map(spectrum[i], 0, 256, 600, 100);
-
         // Peak 1
         var peak1_leftControlX = map(spectrum[i], 0, 256, width*.15, width*.2);
-        var peak1_leftControlY = map(spectrum[i], 0, 256, height*.45, height*.35);
+        var peak1_leftControlY = map(spectrum[i], 0, 256, height*.45, height*.25);
+        var peak1_anchorX = map(spectrum[i], 0, 256, width*.35, width*.4);
+        var peak1_anchorY = map(spectrum[i], 0, 256, height*.85, height*.9);
 
         // Peak 2
         var peak2_leftControlX = map(spectrum[i], 0, 256, width*.55, width*.63);
-        var peak2_leftControlY = map(spectrum[i], 0, 256, height*.75, height*.65);
+        var peak2_leftControlY = map(spectrum[i], 0, 256, height*.75, height*.55);
+        var peak2_anchorX = map(spectrum[i], 0, 256, width*.8, width*.75);
+        var peak2_anchorY = map(spectrum[i], 0, 256, height*.8, height*.85);
 
         // Peak 3
         var peak3_leftControlX = map(spectrum[i], 0, 256, width*.92, width*.98);
-        var peak3_leftControlY = map(spectrum[i], 0, 256, height*.58, height*.48);
+        var peak3_leftControlY = map(spectrum[i], 0, 256, height*.58, height*.38);
 
-        return [peak1_leftControlX,
-                peak1_leftControlY,
-                peak2_leftControlX,
-                peak2_leftControlY,
-                peak3_leftControlX,
-                peak3_leftControlY];
+        return [peak1_leftControlX, //0
+                peak1_leftControlY, //1
+                peak1_anchorX, //2
+                peak1_anchorY, //3
+
+                peak2_leftControlX, //4
+                peak2_leftControlY, //5
+                peak2_anchorX, //6
+                peak2_anchorY, //7
+
+                peak3_leftControlX, //8
+                peak3_leftControlY]; //9
     }
 }
 
 function draw() {
-    //background("rgb(22,25,37)");
-    background(255);
+    background("rgb(22,25,37)");
+    //background(255);
     // Draw FPS (rounded to 2 decimal places) at the bottom left of the screen
     var fps = frameRate();
-    fill(0);
+    fill(255);
     stroke(0);
     text("FPS: " + fps.toFixed(2), 10, 50);
 
-    // Set gradient color scheme
-    // var gradient = ctx.createRadialGradient(width*.14, amplitude(), width, width, height, 0);
+    var dark = color('rgba(11, 10, 10, .65)');
+    var grey = color('rgba(142, 137, 137, .65)');
+    var purple = color('rgb(106, 63, 190)');
+
+    noStroke();
+
+    fill(purple);
+    //Set gradient color scheme
+    // var gradient = ctx.createRadialGradient(width, width, width, width, height, 0);
     // gradient.addColorStop(0, "rgb(48, 35, 174)");
     // gradient.addColorStop(1, "rgb(200,109,215)");
     // ctx.fillStyle = gradient;
 
-    strokeWeight(10)
-    //point(width*.094,height*.62);
-    strokeWeight(1);
-
-
-         // Peak 1
-    //RED
-    stroke(255,0,0);
-    line(width*.35,height*.85, amplitude()[0],amplitude()[1]);
-
-    //GREEN
-    stroke(0,255,0);
-    line(width*.35,height*.85, width*.25,height*.85);
-
-         // Peak 2
-    //RED
-    stroke(255,0,0);
-    line(width*.80,height*.8, width*.55,height*.86);
-
-    //GREEN
-    stroke(0,255,0);
-    line(width*.80,height*.8, amplitude()[2],amplitude()[3]);
-
-
-         // Peak 3
-    //RED
-    stroke(255,0,0);
-    line(width*.98,height*.7, width*.88,height*.85);
-
-    //GREEN
-    stroke(0,255,0);
-    line(width*.98,height*.7, amplitude()[4],amplitude()[5]);
-
-
-    stroke(0);
-    noFill();
-
     beginShape();
     vertex(10,height *.98);
     vertex(10,height * .8);
-    bezierVertex(width*.15,height*.35,  width*.25,height*.85,  width*.35,height*.85); //Peak-1
-    bezierVertex(width*.55,height*.86,  width*.55,height*.65,  width*.80,height*.8); //Peak-2
-    bezierVertex(width*.88,height*.85,  width*.92,height*.48,  width*.98,height*.7); //Peak-3
+    bezierVertex(amplitude()[0],amplitude()[1] -100,  width*.25,height*.85,  amplitude()[2],amplitude()[3]); //Peak-1
+    bezierVertex(width*.55,height*.86,  amplitude()[4],amplitude()[5] - 200,  amplitude()[6],amplitude()[7]); //Peak-2
+    bezierVertex(width*.86,amplitude()[6],  amplitude()[8] - 100,amplitude()[9] -200,  width*.98,height*.7); //Peak-3
     vertex(width * .98,height *.98);
-
     endShape();
-    // beginShape();
-    //
-    // vertex(10,height *.98);
-    // vertex(10,height * .8);
-    // bezierVertex(amplitude()[0],height*.76,  width*.05,height*.6,   amplitude()[1],height*.60);//peak1-left
-    // bezierVertex(amplitude()[2],height*.60,  width*.2,height*.80,   width*.3,height*.90);//peak1-right
-    //
-    // // bezierVertex(width*.4,height*.95,  width*.4,height*.8,   width*.55,height*.82);//peak2-left
-    // // bezierVertex(width*.62,height*.82,  width*.62,height*.95,   width*.8,height*.89);//peak2-right
-    // //
-    // // bezierVertex(width*.95,height*.78,  width*.8,height*.65,   width*.93,height*.6);//peak3-left
-    // // bezierVertex(width*.97,height*.61,  width*.96,height*.67,   width * .98,height*.7);//peak3-right
-    // // vertex(width * .98,height *.98);
-    //
-    // endShape();
+
+    fill(grey);
+    beginShape();
+    vertex(10,height *.98);
+    vertex(10,height * .8);
+    bezierVertex(amplitude()[0],amplitude()[1] - 150,  width*.25,height*.85,  amplitude()[2],amplitude()[3]); //Peak-1
+    bezierVertex(width*.55,height*.86,  amplitude()[4],amplitude()[5] - 100,  amplitude()[6],amplitude()[7]); //Peak-2
+    bezierVertex(width*.86,amplitude()[6],  amplitude()[8],amplitude()[9] - 100,  width*.98,height*.7); //Peak-3
+    vertex(width * .98,height *.98);
+    endShape();
+
+    fill(dark);
+    beginShape();
+    vertex(10,height *.98);
+    vertex(10,height * .8);
+    bezierVertex(amplitude()[0],amplitude()[1],  width*.25,height*.85,  amplitude()[2],amplitude()[3]); //Peak-1
+    bezierVertex(width*.55,height*.86,  amplitude()[4],amplitude()[5],  amplitude()[6],amplitude()[7]); //Peak-2
+    bezierVertex(width*.86,amplitude()[6],  amplitude()[8],amplitude()[9],  width*.98,height*.7); //Peak-3
+    vertex(width * .98,height *.98);
+    endShape();
+
 
 }
