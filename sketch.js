960461d0728e5c651c29ea0274563249b5af2d58
Sumner <serhard2556@gmail.com>
2017-10-15 23:38:39 -0600

    Made a Wave object for the visualizer and converted all code to es6 syntax

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 
 p5.disableFriendlyErrors = true;
 
-var song, fft, button, radius;
+let song, fft;
 
 //Colors
-var dark = color('rgba(11, 10, 10, .65)');
-var grey = color('rgba(142, 137, 137, .65)');
-var purple = color('rgb(106, 63, 190)');
+let dark;
+let grey;
+let purple;
+
+let waves = [];
 
 function toggleSong() {
     if (song.isPlaying()) {
@@ -23,7 +25,7 @@ function preload() {
 
 function setup() {
 
-    var myCanvas = createCanvas(window.innerWidth, window.innerHeight);
+    let myCanvas = createCanvas(window.innerWidth, window.innerHeight);
     myCanvas.parent('visualizer');
 
     fft = new p5.FFT();
@@ -33,9 +35,32 @@ function setup() {
     grey = color('rgba(142, 137, 137, .65)');
     purple = color('rgb(106, 63, 190)');
 
-    button = createButton('toggle');
-    button.mousePressed(toggleSong);
+
     song.play();
+}
+
+// Should be replaces with a function that detects how many songs are playing in the visualizer
+function mousePressed() {
+
+    let wave;
+
+    if (waves.length == 0){
+        wave = new Wave(purple, .45);
+        waves.push(wave);
+    } else if (waves.length == 1){
+        wave = new Wave(grey, .65);
+        waves.push(wave);
+    } else if (waves.length == 2) {
+        wave = new Wave(dark, .75);
+        waves.push(wave);
+    } else if (waves.length == 3) {
+        wave = new Wave(purple, .85);
+        waves.push(wave);
+    } else if (waves.length == 4) { // allow a max of 5 waves
+        wave = new Wave(dark, .95);
+        waves.push(wave);
+    }
+
 }
 
 function windowResized() {
@@ -47,80 +72,17 @@ function draw() {
     noStroke();
 
     // // Draw FPS (rounded to 2 decimal places) at the bottom left of the screen
-    var fps = frameRate();
+    let fps = frameRate();
     fill(255);
     stroke(0);
     text("FPS: " + fps.toFixed(2), 10, 50);
 
-    var spectrum = fft.analyze();
-
-    // scaledSpectrum is a new, smaller array of more meaningful values
-    var scaledSpectrum = splitOctaves(spectrum, 3);
-    var len = scaledSpectrum.length;
-
-    // Wave 1
-    push();
-    fill(purple);
-    noStroke();
-    beginShape();
-    curveVertex(0, height);
-    for (var i = 0; i < len; i++) {
-        var point = smoothPoint(scaledSpectrum, i, 2);
-        var x = map(i, 0, len-1, 0, width);
-        var y = map(point, 0, 255, height, height*.45);
-        curveVertex(x, y);
+    // There can only be a total of 5 visualizers
+    for (let i = 0; i < waves.length; i++){
+        waves[i].show();
     }
-    curveVertex(width, height);
-    endShape(CLOSE);
-    pop();
-
-    // Wave 2
-    push();
-    fill(grey);
-    noStroke();
-    beginShape();
-    curveVertex(0, height);
-    for (var i = 0; i < len; i++) {
-        var point = smoothPoint(scaledSpectrum, i);
-        var x = map(i, 0, len-1, 0, width);
-        var y = map(point, 0, 255, height, height*.65);
-        curveVertex(x, y);
-    }
-    curveVertex(width, height);
-    endShape(CLOSE);
-    pop();
-
-    // Wave 3
-    push();
-    fill(dark);
-    noStroke();
-    beginShape();
-    curveVertex(0, height);
-    for (var i = 0; i < len; i++) {
-        var point = smoothPoint(scaledSpectrum, i, 4);
-        var x = map(i, 0, len-1, 0, width);
-        var y = map(point, 0, 255, height, height*.75);
-        curveVertex(x, y);
-    }
-    curveVertex(width, height);
-    endShape(CLOSE);
-    pop();
 
 }
-
-/**
- *  Divides an fft array into octaves with each
- *  divided by three, or by a specified "slicesPerOctave".
- *
- *  There are 10 octaves in the range 20 - 20,000 Hz,
- *  so this will result in 10 * slicesPerOctave + 1
- *
- *  @method splitOctaves
- *  @param {Array} spectrum Array of fft.analyze() values
- *  @param {Number} [slicesPerOctave] defaults to thirds
- *  @return {Array} scaledSpectrum array of the spectrum reorganized by division
- *                                 of octaves
- */
 
 function playerVisualizer() {
     push();
@@ -128,91 +90,17 @@ function playerVisualizer() {
     stroke(255);
     line(0, height / 2, width, height /2);
 
-    var waveform = fft.waveform();
+    let waveform = fft.waveform();
     noFill();
     beginShape();
     stroke(255);
     strokeWeight(1);
-    for (var i = 0; i< waveform.length; i++){
-        var x = map(i, 0, waveform.length, 0, width);
-        var y = map( waveform[i], -1, 1, 0, height);
+    for (let i = 0; i< waveform.length; i++){
+        let x = map(i, 0, waveform.length, 0, width);
+        let y = map( waveform[i], -1, 1, 0, height);
         vertex(x,y);
     }
     endShape();
 
     pop();
-}
-
-function splitOctaves(spectrum, slicesPerOctave) {
-    var scaledSpectrum = [];
-    var len = spectrum.length;
-
-    // default to thirds
-    var n = slicesPerOctave|| 3;
-    var nthRootOfTwo = Math.pow(2, 1/n);
-
-    // the last N bins get their own
-    var lowestBin = slicesPerOctave;
-
-    var binIndex = len - 1;
-    var i = binIndex;
-
-
-    while (i > lowestBin) {
-        var nextBinIndex = round( binIndex/nthRootOfTwo );
-
-        if (nextBinIndex === 1) return;
-
-        var total = 0;
-        var numBins = 0;
-
-        // add up all of the values for the frequencies
-        for (i = binIndex; i > nextBinIndex; i--) {
-            total += spectrum[i];
-            numBins++;
-        }
-
-        // divide total sum by number of bins
-        var energy = total/numBins;
-        scaledSpectrum.push(energy);
-
-        // keep the loop going
-        binIndex = nextBinIndex;
-    }
-
-    // add the lowest bins at the end
-    for (var j = i; j > 0; j--) {
-        scaledSpectrum.push(spectrum[j]);
-    }
-
-    // reverse so that array has same order as original array (low to high frequencies)
-    scaledSpectrum.reverse();
-
-    return scaledSpectrum;
-}
-
-// average a point in an array with its neighbors
-function smoothPoint(spectrum, index, numberOfNeighbors) {
-
-    // default to 2 neighbors on either side
-    var neighbors = numberOfNeighbors || 2;
-    var len = spectrum.length;
-
-    var val = 0;
-
-    // start below the index
-    var indexMinusNeighbors = index - neighbors;
-    var smoothedPoints = 0;
-
-    for (var i = indexMinusNeighbors; i < (index+neighbors) && i < len; i++) {
-        // if there is a point at spectrum[i], tally it
-        if (typeof(spectrum[i]) !== 'undefined') {
-            val += spectrum[i];
-            smoothedPoints++;
-        }
-    }
-
-    val = val/smoothedPoints;
-
-    return val;
 }
--- wave.js
+++ wave.js
@@ -1,87 +1,115 @@
-// Wave Class
-function Wave (color, posX, posY, limiter, flipWave) {
+class Wave {
+    constructor(color, adjuster) {
+        this.color = color;
+        this.adjuster = adjuster;
+    }
 
-    this.mColor = color;
-    this.mPosX = posX;
-    this.mPosY = posY;
-    this.mLimiter = limiter;
-    this.mFlipWave = flipWave;
-    this.yoff = 0.0;
-
-
-    this.waveSpectrum = function () {
+    show() {
         this.spectrum = fft.analyze();
-        return this.spectrum;
-    }
 
-    this.show = function (amp) {
+        this.scaled = this.splitOctives(this.spectrum, 3);
+        this.len = this.scaledSpectrum.length;
 
+        push();
+        fill(this.color);
+        noStroke();
         beginShape();
-            stroke(this.mColor);
-            strokeWeight(10);
-            smooth();
-            this.position();
-            this.waveSpectrum();
-
-                // if (this.mFlipWave){
-                //    this.flipWaveHorizontally();
-                //    //console.log("flip");
-                // } else {
-                    this.amplitude();
-                //}
-        endShape();
-
-
+        curveVertex(0, height);
+        for (let i = 0; i < this.len; i++) {
+            this.point = this.smoothPoint(this.scaled, i, 2);
+            this.x = map(i, 0, this.len-1, 0, width);
+            this.y = map(this.point, 0, 255, height, height * this.adjuster);
+            curveVertex(this.x, this.y);
+        }
+        curveVertex(width, height);
+        endShape(CLOSE);
+        pop();
     }
 
-    // this.vertexWave = function () {
-    //
-    //     var spectrum = mfft.analyze();
-    //
-    //     beginShape();
-    //     for (i = 0; i<spectrum.length; i++) {
-    //         vertex(i, map(spectrum[i], 0, 255, height, 0) );
-    //     }
-    //     endShape();
-    // }
-    //
-    this.position = function () {
-        translate(this.mPosX, this.mPosY);
-    }
+    /**
+     *  Divides an fft array into octaves with each
+     *  divided by three, or by a specified "slicesPerOctave".
+     *
+     *  There are 10 octaves in the range 20 - 20,000 Hz,
+     *  so this will result in 10 * slicesPerOctave + 1
+     *
+     *  @method splitOctaves
+     *  @param {Array} spectrum Array of fft.analyze() values
+     *  @param {Number} [slicesPerOctave] defaults to thirds
+     *  @return {Array} scaledSpectrum array of the spectrum reorganized by division
+     *                                 of octaves
+     */
 
-    /* Set the amplitude of each line based on the frequency spectrum
-       of that given time domain */
-    this.amplitude = function () {
+    splitOctives(spectrum, slicesPerOctave){
+        this.scaledSpectrum = [];
+        this.len = spectrum.length;
 
-        //console.log("spectrum" + this.waveSpectrum.value());
+        // default to thirds
+        this.n = slicesPerOctave|| 3;
+        this.nthRootOfTwo = Math.pow(2, 1/this.n);
 
-        for (var i = 0; i < this.waveSpectrum().length; i++) {
+        // the last N bins get their own
+        this.lowestBin = slicesPerOctave;
 
-            this.amp = this.waveSpectrum()[i];
+        this.binIndex = this.len - 1;
+        this.i = this.binIndex;
 
-            this.y = map(this.amp/this.mLimiter, 0, 256, height, 0);
 
-            line(i * 50, height * 2, i * 50, this.y);
+        while (this.i > this.lowestBin) {
+            this.nextBinIndex = round( this.binIndex/this.nthRootOfTwo );
 
-           //ellipse(i * 50, this.y, i * 50, height);
+            if (this.nextBinIndex === 1) return;
+
+            this.total = 0;
+            this.numBins = 0;
+
+            // add up all of the values for the frequencies
+            for (this.i = this.binIndex; this.i > this.nextBinIndex; this.i--) {
+                this.total += spectrum[this.i];
+                this.numBins++;
+            }
+
+            // divide total sum by number of bins
+            this.energy = this.total/this.numBins;
+            this.scaledSpectrum.push(this.energy);
+
+            // keep the loop going
+            this.binIndex = this.nextBinIndex;
         }
 
-        //ellipse(i * 50, this.y, i * 50, height);
-        return this.amp;
+        // add the lowest bins at the end
+        for (let j = this.i; j > 0; j--) {
+            this.scaledSpectrum.push(this.spectrum[j]);
+        }
+
+        // reverse so that array has same order as original array (low to high frequencies)
+        this.scaledSpectrum.reverse();
+
+        return this.scaledSpectrum;
     }
 
-    // this.flipWaveHorizontally = function () {
-    //
-    //     for (var j = this.waveSpectrum().length; j > 0; j--) {
-    //
-    //         this.amp = this.waveSpectrum()[j];
-    //
-    //         this.y = map(this.amp/this.mLimiter, 0, 256, height, 0);
-    //
-    //         line(j, height, j, this.y);
-    //     }
-    //     return this.amp;
-    //
-    // }
+    smoothPoint(spectrum, index, numberOfNeighbors){
+        // default to 2 neighbors on either side
+        this.neighbors = numberOfNeighbors || 2;
+        this.len = spectrum.length;
+
+        this.val = 0;
+
+        // start below the index
+        this.indexMinusNeighbors = index - this.neighbors;
+        this.smoothedPoints = 0;
+
+        for (let i = this.indexMinusNeighbors; i < (index+this.neighbors) && i < this.len; i++) {
+            // if there is a point at spectrum[i], tally it
+            if (typeof(spectrum[i]) !== 'undefined') {
+                this.val += spectrum[i];
+                this.smoothedPoints++;
+            }
+        }
+
+        this.val = this.val/this.smoothedPoints;
+
+        return this.val;
+    }
 
 }
